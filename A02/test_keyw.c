62e07d7be60e305ca8ee78063999f0a9adcf56b9
Andrei Rabusov <arabusov@gmail.com>
2025-04-20 19:51:26 +0200

    Add negative test for keywords

--- A02/test_keyw.c
+++ A02/test_keyw.c
@@ -7,10 +7,14 @@ static struct ktest {
 	int res;
 	enum keyw class;
 } tests[] = {
+	{ "alignas", FALSE },	/* C23 */
+	{ "alignof", FALSE },	/* C23 */
 	{ "auto", TRUE, AUTO },
+	{ "bool", FALSE },	/* C23 */
 	{ "break", TRUE, BREAK },
 	{ "case", TRUE, CASE },
 	{ "const", TRUE, CONST },
+	{ "constexpr", FALSE },	/* C23 */
 	{ "continue", TRUE, CONTINUE },
 	{ "default", TRUE, DEFAULT },
 	{ "do", TRUE, DO },
@@ -18,26 +22,38 @@ static struct ktest {
 	{ "else", TRUE, ELSE },
 	{ "enum", TRUE, ENUM },
 	{ "extern", TRUE, EXTERN },
+	{ "false", FALSE },	/* C23 */
 	{ "float", TRUE, FLOAT },
 	{ "for", TRUE, FOR },
 	{ "goto", TRUE, GOTO },
 	{ "if", TRUE, IF },
 	{ "int", TRUE, INT },
+	{ "inline", FALSE },	/* C99 */
 	{ "long", TRUE, LONG },
+	{ "nullptr", FALSE },	/* C23 */
 	{ "register", TRUE, REGISTER },
 	{ "return", TRUE, RETURN },
+	{ "restrict", FALSE },	/* C99 */
 	{ "short", TRUE, SHORT },
 	{ "signed", TRUE, SIGNED },
 	{ "sizeof", TRUE, SIZEOF },
 	{ "static", TRUE, STATIC },
+	{ "static_assert", FALSE },	/* C23 */
 	{ "struct", TRUE, STRUCT },
 	{ "switch", TRUE, SWITCH },
+	{ "thread_local", FALSE },	/* C23 */
+	{ "true", FALSE },	/* C23 */
 	{ "typedef", TRUE, TYPEDEF },
+	{ "typeof", FALSE },	/* C23 */
+	{ "typeof_unqual", FALSE },	/* C23 */
 	{ "union", TRUE, UNION },
 	{ "unsigned", TRUE, UNSIGNED },
 	{ "void", TRUE, VOID },
 	{ "volatile", TRUE, VOLATILE },
 	{ "while", TRUE, WHILE },
+	{ "_Bool", FALSE },	/* C99 */
+	{ "_Complex", FALSE },	/* C99 */
+	{ "_Imaginary", FALSE },	/* C99 */
 };
 
 static int single_kwtest(struct ktest *tup)
